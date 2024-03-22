1dd9bba9010353a99e3c686f70323fedc0ca5cb8
aimansae <saeedaimann@gmail.com>
2024-03-22 12:39:06 +0100

    Removed fixed height from Card.tsx

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -3,7 +3,7 @@ import { MyForm } from '../MyForm';
 const Card = () => {
   return (
     <div className="flex h-screen w-full overflow-y-hidden p-2 font-sans ">
-      <div className="shadow-gray-600 m-auto grid h-[550px] w-[500px] grid-cols-1 gap-6 bg-white shadow-lg sm:w-[900px] md:grid-cols-2">
+      <div className="shadow-gray-600 m-auto grid w-[500px] grid-cols-1 gap-6 bg-white shadow-lg sm:w-[900px] md:grid-cols-2">
         <HeroImage />
         <MyForm />
       </div>
